720221548ea6cb5edb9d2e0179f191b663b1eb2a
Maykon Nazareth <104942943+mayconnazareth@users.noreply.github.com>
2025-08-12 09:24:54 -0300

    Impressão do Resultado
    Altera como o resultado é mostrado.

--- potencia.rb
+++ potencia.rb
@@ -7,4 +7,14 @@ numeros.insert(1, gets.chomp.to_i)
 print "Digite um numero: "
 numeros.insert(2, gets.chomp.to_i)
 
-puts numeros.map { |numero| numero ** 3 }
+puts "\nResultados:"
+puts "=" * 30
+
+numeros.each do |numero|
+  resultado = numero ** 3
+  puts "#{numero}³ = #{resultado}"
+end
+
+puts "=" * 30
+puts "Array completo: #{numeros}"
+puts "Array com potências: #{numeros.map { |n| n ** 3 }}"
